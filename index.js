7a7a66331ffa559678e0cdd33f1528fb349efba5
AntrikshNarang <itsantriksh@gmail.com>
2022-12-22 10:05:45 +0530

    Added Win Messages, website challenge completed

--- index.js
+++ index.js
@@ -36,3 +36,12 @@ else if(dice2 === 5){
 else{
     document.querySelector(".img2").setAttribute("src","images/dice6.png");
 }
+if(dice1 > dice2){
+    document.querySelector(".container h1").textContent=("🚩Player 1 Wins");
+}
+else if(dice2 > dice1){
+    document.querySelector(".container h1").textContent=("Player 2 Wins🚩");
+}
+else{
+    document.querySelector(".container h1").textContent=("It's a Tie");
+}
